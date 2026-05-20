4339cb5505f19dfb9d9e93af410e91a60193e20a
Elyas Edris <elyasedris759@gmail.com>
2026-05-20 19:09:02 +0430

    new contents add and removed the bugs

--- app/data.jsx
+++ app/data.jsx
@@ -2,14 +2,14 @@ export const data = [
     {
     id: 1, 
     name: "Brain MRI",
-    image: "/radiology.jpg",
+    image: "/brain.jpg",
     desc: "High quality MRI scanning for brain diagnosis.",
     
     },
     {
     id: 2, 
     name: "Spine MRI",
-    image: "/radio.jpg",
+    image: "/spine.jpg",
     desc: "Detailed spine imagine with advanced technology.",
     
 
@@ -19,24 +19,44 @@ export const data = [
 export const background =[
     {
     
-    image: "/radiology.jpg",
+    image: "/mri_machine.jpg",
     },
     {
     
-    image: "/radio.jpg"
+    image: "/mri_machine_1.jpg"
     },
+    {
+    
+    image: "/mri_machine_2.jpg",
+    },
+    {
+    
+    image: "/mri_machine_3.jpg"
+    },
+    {
+    
+    image: "/mri_machine_4.jpg",
+    },
+    {
+    
+    image: "/mri_machine_5.jpg"
+    },
+    {
+    
+    image: "/mri_machine_6.jpg",
+    }
 ]
 export const ctsccandata = [
    {
     id: 1, 
     name: "Chest CT Scan",
-    image: "/radiology.jpg",
+    image: "/chest.jpg",
     desc: "Accurate chest imaging for medical diagnosis.",
     },
     {
     id: 2, 
     name: "Abdomen CT Scan",
-    image: "/radio.jpg",
+    image: "/mri_machine_1.jpg",
     desc: "Modern CT scan services with expert radiologists.",
     }, 
 ]
